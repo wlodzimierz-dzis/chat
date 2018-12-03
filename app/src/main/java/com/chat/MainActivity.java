29454032e19903ce1624596da3698de1f96fe5d7
Mordurin <Ulan04041995@wp.pl>
2018-12-03 14:15:10 +0100

    commit 1

--- app/src/main/java/com/chat/MainActivity.java
+++ app/src/main/java/com/chat/MainActivity.java
@@ -1,7 +1,10 @@
 package com.chat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.buttonMainActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { MainActivity.this.createNewActivity(v); }
+        });
+
+    }
+
+    public void createNewActivity(View v){
+        startActivity(new Intent(MainActivity.this, Conversation_list.class));
     }
 }
