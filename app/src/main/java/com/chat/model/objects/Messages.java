54d9a9f5401fb30d8feea05041ad6e2f7ff50883
Mordurin <Ulan04041995@wp.pl>
2018-12-03 19:09:32 +0100

    commit 3 + objects

---
+++ app/src/main/java/com/chat/model/objects/Messages.java
@@ -0,0 +1,33 @@
+package com.chat.model.objects;
+
+public class Messages {
+    private int message_id;
+    private int contact_id;
+    private int conversation_id;
+    private String content;
+    private String geolocation;
+
+    public Messages(String content, String geolocation, int contact_id, int conversation_id) {
+        this.content = content;
+        this.geolocation = geolocation;
+        this.contact_id = contact_id;
+        this.conversation_id = conversation_id;
+    }
+
+    public int getId() {
+        return message_id;
+    }
+    public void setId(int id)  { this.message_id = id; }
+
+    public String getContent() {
+        return content;
+    }
+    public String getGeolocation() {
+        return geolocation;
+    }
+
+    public String toString() {
+        return  content + ' ' + geolocation ;
+    }
+}
+
