586e6087e20bc1a76f90f755220fe74a7cc9b5a2
Volodymyr Dzis <volodymyr.dzis@miquido.com>
2018-12-04 09:09:50 +0100

    Interfaces

--- app/src/main/java/com/chat/MainActivity.java
+++ app/src/main/java/com/chat/MainActivity.java
@@ -1,7 +1,15 @@
 package com.chat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter studentsAdapter = setAdapter();
+        ListView listView = findViewById(R.id.chatsList);
+        listView.setAdapter(studentsAdapter);
+    }
+    private ArrayAdapter setAdapter() {
+        String chats[] = {"Bartosz", "Tomasz", "Konrad"};
+        ArrayList<String> chatsList = new ArrayList<String>();
+        chatsList.addAll( Arrays.asList(chats) );
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.activity_main, R.id.chat_textlist, chatsList);
+
+        return adapter;
+    }
+
+    public void onItemClick( View view) {
+        Intent i = new Intent(MainActivity.this, chat.class);
+        startActivity(i);
     }
 }
